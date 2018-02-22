976af7dfbb3f48357f67fc1936c91d0f6990576c
Howard Zheng <ycwei982@outlook.com>
2018-02-22 20:56:47 +0800

    Update colors

--- flash.py
+++ flash.py
@@ -6,7 +6,7 @@ long_y = os.get_terminal_size().lines
 
 while True:
     for i in range(0, long_x * long_y):
-        sys.stdout.write("\033[0m#")
+        sys.stdout.write("\033[1;30;37m#")
         i = i + 1
     i = 0
     for i in range(0,long_x*long_y):
